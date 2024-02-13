5a1363c1da6fe0bcfc7feb2b8ddd3960b036bcd4
Anubhav Gupta <mail.anubhav06@gmail.com>
2024-02-13 23:24:54 +0530

    create frontend
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage';
+
+import Modal from 'react-modal';
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen overflow-x-hidden">
+      <Router>
+        <Routes>
+          <Route element={<LandingPage />} path='/' />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext } from 'react'
+
+const LandingPage = () => {
+
+    return (
+        <div>
+            <h1> Library Management System </h1>
+        </div>
+    )
+}
+
+export default LandingPage
